2d521b7b99d5d979cf4591c8fb0b07bd34f4f1b6
coolejubo <al.huubw@gmail.com>
2023-02-01 10:49:25 +0100

    AJAX likesyteem

--- backEnd/includes/commentchains.php
+++ backEnd/includes/commentchains.php
@@ -6,6 +6,7 @@
         exit('no id');
     }
 
+    // functie om een reply chain te maken, dit is een lijst met alle replies op een comment
     function createReplyChain($parentID, $replies) {
         $reply_chain = array();
         foreach($replies as $reply) {
@@ -20,6 +21,7 @@
         return $reply_chain;
     }
     
+    // maakt een array om alle comments in op te slaan
     $postID = (int) $_GET['id'];
     $result = $connection->query("SELECT r.responseID, r.parentID, r.responseContent, r.responseDate, r.postID, r.responseLikes, u.username FROM responses r JOIN users u ON r.userID = u.userID WHERE r.postID = '$postID' ORDER BY r.parentID, r.responseDate DESC");
     $replies = array();
@@ -34,6 +36,8 @@
             "username" => $row['username']
         );
     }
+
+    // maakt een reply chain voor alle top comments (comments op de post zelf)
     $reply_chains = createReplyChain(0, $replies);
     $connection->close();
 
--- backEnd/includes/createComment.inc.php
+++ backEnd/includes/createComment.inc.php
@@ -11,17 +11,19 @@ if (!isset($_SESSION["userID"])) {
     require_once 'functions.inc.php';
     require_once 'connection.php';
 
-    $responseContent = $_POST['responseContent'];
-    $postID = $_POST['postID'];
-    $userID = $_SESSION["userID"];
-    $parentID = $_POST['parentID'];
 
+    // verdeelt alle eigenschappen van een comment over de juiste variablen en sanitized de comments
+    $responseContent = htmlspecialchars($_POST['responseContent']);
+    $postID = htmlspecialchars($_POST['postID']);
+    $userID = $_SESSION["userID"];
+    $parentID = htmlspecialchars($_POST['parentID']);
+
+    // voegt de comment toe aan de database
     $connection->query("INSERT INTO responses (responseContent, postID, userID, parentID) VALUES ('$responseContent', '$postID', '$userID', '$parentID')");
 
     $connection->close();
 
     header("Location: https://webtech-ki46.webtech-uva.nl/frontEnd/threads/posts.php?id=" . $postID);
     exit();
-    // huub stinkt
 
 ?>
